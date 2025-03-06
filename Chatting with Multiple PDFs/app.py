25f6eb378566af2e52d3f347f54be80551cf577c
Abhivyakt Bhati <abhivyaktbhati1102@gmail.com>
2025-03-06 22:57:04 +0530

    minor changes

--- Chatting with Multiple PDFs/app.py
+++ Chatting with Multiple PDFs/app.py
@@ -86,6 +86,3 @@ def main():
                 text_chunks = get_text_chunks(raw_text) 
                 get_vector_store(text_chunks)
                 st.success("Done")
-
-if __name__ == "__main__":
-    main()
